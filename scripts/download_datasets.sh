873e3083f465e6c239e641aa3b77e8ed280e8082
fwu-asapp <56092571+fwu-asapp@users.noreply.github.com>
2022-02-04 07:20:59 -0800

    Update download_datasets.sh

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -12,3 +12,10 @@ tar -xzvf datasets/slue-voxpopuli_blind.tar.gz -C datasets/
 
 python slue_toolkit/prepare/prepare_voxceleb.py create_manifest
 python slue_toolkit/prepare/prepare_voxpopuli.py create_manifest
+
+#4. create dict
+for split in voxceleb voxpopuli; do
+for label in ltr wrd; do
+    python slue_toolkit/prepare/create_dict.py manifest/slue-${split}/fine-tune.${label} manifest/slue-${split}/dict.${label}.txt
+done
+done
